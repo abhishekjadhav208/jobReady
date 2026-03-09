5e579dc9e72f84b5191748d64e1fc68d4890477e
abhishekjadhav208 <abhishekjadhav5035@gmail.com>
2026-03-10 02:04:08 +0530

    fix case sensitive import

--- frontend/src/pages/CourseModule.jsx
+++ frontend/src/pages/CourseModule.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchAllModules, fetchAllLesson } from "../redux/features/Course/courseSlice";
-import { markLessonComplete, getProgress } from "../redux/features/Progress/ProgressSlice";
+import { markLessonComplete, getProgress } from "../redux/features/Progress/progressSlice";
 import { useParams } from "react-router-dom";
 
 const CourseModule = () => {
